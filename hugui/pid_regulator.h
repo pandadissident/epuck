fe1ce8692003702d45a36531497ea5408f56de23
hugodirat <hugo.dirat@hotmail.fr>
2020-04-23 18:55:51 +0200

    mise en place des régulateurs pour controler les moteurs en fonction de l'alignement et de l'angle de tangage, il reste a mettre les bonnes valeurs numerique dans les #defines

--- hugui/pid_regulator.h
+++ hugui/pid_regulator.h
@@ -9,26 +9,29 @@ extern messagebus_t bus;
 extern parameter_namespace_t parameter_root;
 
 //constants for the differents parts of the project
-#define IMAGE_BUFFER_SIZE		640
-#define WIDTH_SLOPE				5
-#define MIN_LINE_WIDTH			40
+//#define IMAGE_BUFFER_SIZE		640
+//#define WIDTH_SLOPE				5
+//#define MIN_LINE_WIDTH			40
 #define ROTATION_THRESHOLD		10
 #define ROTATION_COEFF			2
-#define PXTOCM					1570.0f //experimental value
-#define GOAL_DISTANCE 			10.0f
-#define MAX_DISTANCE 			25.0f
-#define ERROR_THRESHOLD			0.1f	//[cm] because of the noise of the camera
-#define KP						800.0f
-#define KI 						3.5f	//must not be zero
-#define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
-
+//#define PXTOCM					1570.0f //experimental value
+//#define GOAL_DISTANCE 			10.0f
+//#define MAX_DISTANCE 			25.0f
+#define ERROR_THRESHOLD_ALIGN	0.1f
+#define ERROR_THRESHOLD_ANGLE	0.1f//[cm] because of the noise of the camera
+#define KP_ALIGN				800.0f
+#define KP_ANGLE				800.0f
+#define KI_ALIGN				3.5f
+#define KI_ANGLE				3.5f	//must not be zero
+#define MAX_SUM_ERROR_ALIGN		(MOTOR_SPEED_LIMIT/KI_ALIGN)
+#define MAX_SUM_ERROR_ANGLE		(MOTOR_SPEED_LIMIT/KI_ANGLE)
 /*! @brief
  *
  *  @param
  *  @param values
  *  @warning
  */
-void straight_line(void);
+//void straight_line(void);
 
 /*! @brief
  *
